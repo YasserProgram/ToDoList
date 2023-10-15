aadfdaa9ee0570c2b7264e943eef2dd00f08a4e8
YasserProgram <137318237+YasserProgram@users.noreply.github.com>
2023-10-15 07:50:56 -0500

    Update main.py
    Comentar el mainloop

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ eliminar_todas_btn = tk.Button(ventana, text="Eliminar todas las tareas", comman
 marcar_todas_btn.pack(side=tk.TOP, padx=10, pady=5, anchor='center')
 eliminar_todas_btn.pack(side=tk.TOP, padx=10, pady=5, anchor='center')
 
+
 ventana.mainloop()
 
 
